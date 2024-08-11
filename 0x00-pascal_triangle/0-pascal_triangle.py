9969acce0819f8724cedda1c3ff0193c14fed44d
EngineerLucky <lucksenhlatshwayo19@gmail.com>
2024-08-11 12:41:26 -0700

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,24 +5,13 @@ Pascal's Triangle - Combined Approach
 
 def pascal_triangle(n):
     """Returns a list of lists of integers representing Pascal’s triangle of n."""
-    if n <= 0:
-        return []
-
-    res = [[1]]  # Start with the first row
-
-    for i in range(1, n):
-        prev_row = res[-1]  # Get the last row added to the result list
-        curr_row = [1]  # Start the current row with a 1
-
-        # Fill in the middle values of the row
-        for j in range(1, i):
-            curr_row.append(prev_row[j - 1] + prev_row[j])
-
-        curr_row.append(1)  # End the row with a 1
-        res.append(curr_row)  # Add the current row to the result list
-
-    return res  # Return the full Pascal's Triangle
-
-# Example usage:
-print(pascal_triangle(5))
-
+ res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
